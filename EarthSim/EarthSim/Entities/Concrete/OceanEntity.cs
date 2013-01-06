12f58dbc142355112c514a091c4ca316ea8e9943
Johan Dahlberg <info@johandahlberg.com>
2013-01-06 20:45:11 +0100

    Solving crash in OceanEntity

--- EarthSim/EarthSim/Entities/Concrete/OceanEntity.cs
+++ EarthSim/EarthSim/Entities/Concrete/OceanEntity.cs
@@ -5,6 +5,7 @@ using System.Text;
 using EarthSim.Entities.Abstract;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using EarthSim.Utils;
 
 namespace EarthSim.Entities.Concrete
 {
@@ -13,7 +14,7 @@ namespace EarthSim.Entities.Concrete
         public OceanEntity(Game game, float radius, Texture2D texture)
             : base(game, radius, texture)
         {
-            //LoadHeightMap(texture);
+            LoadHeightMap(texture);
 
             // Initialize
             initializeVertices();
@@ -21,6 +22,24 @@ namespace EarthSim.Entities.Concrete
             FixNormals();
             //ConvertToSphere(radius, width, height);
             updateData();
+        }
+
+        private void LoadHeightMap(Texture2D heightMap)
+        {
+            height = heightMap.Height;
+            width = heightMap.Width;
+
+            Color[] heightMapColorInfo = new Color[width * height];
+            heightMap.GetData(heightMapColorInfo);
+            heightData = new HSL[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    heightData[x, y] = RGB2HSL.ToHSL(heightMapColorInfo[x + y * width]);
+                }
+            }
         }
     }
 }
